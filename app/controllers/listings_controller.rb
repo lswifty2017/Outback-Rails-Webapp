9609639afcf8614695a089f6088eb95da32c4362
Liam <l.swift94@gmail.com>
2019-04-29 16:35:30 +1000

    index and show html working

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,24 @@
 class ListingsController < ApplicationController
+  def index
+    @listings = Listing.all
+  end
+  
+  def create
+    @listing = Listing.new(listing_params)
+  end
+
+  def show
+    @listing = Listing.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+  def listing_params
+    params.permit(:title, :location, :description, :cost)
+  end
 end
