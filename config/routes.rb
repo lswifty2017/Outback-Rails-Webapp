e1bad351a0f76ca9507899842c2eb3af05d9c5f7
Liam <l.swift94@gmail.com>
2019-04-30 12:34:34 +1000

    edit is working

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/listings", to: "listings#index", as: "listings"
   get "/listings/:id", to: "listings#show", as: "listing"
-  post "/listings", to: "listings#create", as:"create_listing"
+  post "/listings", to: "listings#create", as: "create_listing"
+  get '/listings/:id/edit', to: 'listings#edit', as: 'edit_listing'
+  patch '/listings/:id/edit', to: 'listings#update', as: 'update_listing'
   root 'listings#index'
 end
