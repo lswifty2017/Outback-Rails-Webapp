62af7e1dadbffdc12fb0eb37e784c34fd9d08783
root <root@Alex-W.localdomain>
2019-05-02 15:56:47 +1000

    Delete Booking system added + email notifications

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -3,4 +3,12 @@ class BookingMailer < ApplicationMailer
     @user = params[:user]
     mail(to: @user.email, subject: 'You have made a new booking!')
   end
-end
+
+
+  def cancelled_booking_email
+    @user = params[:user]
+    @host = params[:host]
+    @booking = params[:booking]
+    mail(to: @host.email, subject: "Camper #{@user} has cancelled their stay at #{@booking"
+  end
+end 
