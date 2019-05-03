040ab36743761d417d1379a8ff6c4b9f53fdb84c
Liam <l.swift94@gmail.com>
2019-05-03 13:21:42 +1000

    can calculate total cost for camping

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,6 +3,7 @@ class BookingsController < ApplicationController
   end
 
   def create
+    
     @listing = params[:listing]
     @booking = Booking.new({
       user_id: current_user.id,
@@ -11,13 +12,17 @@ class BookingsController < ApplicationController
       end_time: params[:end_time]
     })
     @booking.listing.update_attribute(:booked_status, true)
-    if @booking.save
+    if @booking.save!
       @user = current_user
       BookingMailer.with(user: @user).new_booking_email.deliver_now
-      redirect_to listings_path
+      redirect_to :booking_payment
     else
       redirect_to :root
     end
+  end
+  
+  def show
+    raise
   end
 
   def show_user_bookings
@@ -25,11 +30,19 @@ class BookingsController < ApplicationController
     @user = current_user
   end
 
+  def booking_payment
+    @booking = Booking.last
+    @day_cost = @booking.listing.cost
+    @total_days = @booking.amount_of_days
+    @total_cost = @day_cost * @total_days
+    render "booking_payment"
+  end
+
   def edit
   end
 
   def destroy
-    @booking  = Booking.find(params[:id])
+   @booking  = Booking.find(params[:id])
    @booking.listing.update_attribute(:booked_status, false)
    @user = current_user
    @host_user_id = @booking.listing.user_id
